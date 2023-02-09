8b4997cec460627d86a961a2155bb22a75bcf7d4
jam0119 <73720359+jam0119@users.noreply.github.com>
2023-02-08 18:49:20 -0600

    add login page prototype and main
    it wouldn't let me upload the whole project, so here are the two relevant files.

---
+++ login.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+
+def openLogin():
+    root = Tk()
+    root.geometry("1280x720")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+
+    usernameEntry = ttk.Entry(root, width=40)
+
+    passwordEntry = ttk.Entry(root, width=40, show='*')
+    passwordEntry.place(x=550, y=320)
+    usernameLabel = ttk.Label(root, text="Username:")
+    usernameLabel.place(x=480, y=280)
+    passwordLabel = ttk.Label(root, text="Password:")
+    passwordLabel.place(x=480, y=320)
+    welcomeLabel = ttk.Label(root, text="Welcome to Patient Information Management", font=("Arial", 35))
+    welcomeLabel.place(x=200, y=100)
+
+    def login():
+        print(usernameEntry.get())
+        usernameEntry.delete(first=0, last=tk.END)
+        print(passwordEntry.get())
+        passwordEntry.delete(first=0, last=tk.END)
+
+    button = ttk.Button(root, text="Log In", command=login)
+    button.place(x=625, y=370)
+    root.mainloop()
+
+
