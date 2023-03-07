2c127a93a969067fb8fa2341820006c8f7856749
Maraiah Matson <mi.matson42@gmail.com>
2023-03-06 18:26:01 -0700

    added a couple of query functions. added consts.py for better code management

---
+++ consts.py
@@ -0,0 +1,8 @@
+# DO NOT COMMIT THIS LINK TO THE REPO OR ANYONE WHO GRABS IT
+# CAN MESS WITH DATA IN THE DB
+# will look into a more permanent solution.
+HOST = "get link from discord"
+PORT = 3306
+USER = 'admin'
+PASSWORD = 'AdminPass'
+DATABASE = "patient_information"
--- database.py
+++ database.py
@@ -2,30 +2,36 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import pymysql
+from consts import *
+
+
 class Database:
+    def __init__(self):
+        connection = pymysql.connect(host=HOST, user=USER, port=PORT,
+                                     passwd=PASSWORD, db=DATABASE)
+        self.cursor = connection.cursor()
 
-    def __init__(self, host):
-        #host = host                                    # DO NOT COMMIT THIS LINK TO THE REPO OR ANYONE WHO GRABS IT
-                                                            # CAN MESS WITH DATA IN THE DB
-                                                            # will look into a more permanent solution.
-        port = 3306
-        user = 'admin'
-        password = 'AdminPass'
-        database = "patient_information"
+    def execute(self, query):
+        self.cursor.execute(query)
+        output = self.cursor.fetchall()
+        print(format(output))
+        return output
 
-        self.connection = pymysql.connect(host=host, user=user,
-                                          port=port, passwd=password, db=database)
+    def select(self, arg1, arg2):
+        self.cursor.execute("SELECT " + arg1 + " FROM " + arg2 + ";")  # execute query
+        output = self.cursor.fetchall()  # puts output into a string
+        print(format(output))  # prints output to console
+        return output
 
-    #simple skeleton for a select function
-    def select(self):
-        with self.connection:
-            cur = self.connection.cursor()
-            cur.execute("SELECT * FROM doctors;")  # queries the doctors table
-            output = cur.fetchone()                # puts output into a string
-            print(format(output))                  # prints output to console
+    def select_all(self, arg1):
+        self.cursor.execute("SELECT * FROM " + arg1 + ";")  # queries the doctors table
+        output = self.cursor.fetchall()  # puts output into a string
+        print(format(output))  # prints output to console
+        return output
 
 
-poggers = Database("get the link from the discord")
-poggers.select()
-
+poggers = Database()
+poggers.execute("SELECT * from doctors")
+poggers.select('*', 'doctors')
+poggers.select_all('doctors')
 
