7912fdd427084e676e78154b0e194eb2c3f088e6
Michael Kelly <cybernationnews@gmail.com>
2023-03-22 16:37:59 -0500

    Add ability to view account permissions

--- database_tools.py
+++ database_tools.py
@@ -12,20 +12,59 @@ def delete_account(username):
     poggers.commitChanges()
 
 
-def display_accounts():
+def display_accounts(permissions=False):
     accounts = poggers.select('*', 'mysql.user')
-    print("Database Accounts:")
+    privileges = poggers.execute("SELECT * FROM information_schema.user_privileges")
+
     for account in accounts:
         print(account[1])
+
+        if permissions:
+            key = "'" + account[1] + "'@'" + account[0] + "'"
+            for privilege in privileges:
+                if key == privilege[0]:
+                    print("\t" + privilege[-2] + ": " + privilege[-1])
+
     print("")
 
 
-# Test code
-print("***Before account creation***")
-display_accounts()
-print("***After account creation***")
-create_account("test", 123)
-display_accounts()
-print("***After account deletion***")
-delete_account("test")
-display_accounts()
+# TODO: Put test cases in separate file
+def account_creation_test():
+    print("***Before account creation***")
+    display_accounts()
+    print("***After account creation***")
+    create_account("test", 123)
+    display_accounts()
+    print("***After account deletion***")
+    delete_account("test")
+    display_accounts()
+
+
+def create_table(table_name, data_dict={}):
+    poggers.execute("CREATE TABLE " + str(table_name) + " (test_id INT);")
+    poggers.commitChanges()
+
+
+def delete_table(table_name):
+    poggers.execute("DROP TABLE " + str(table_name) + ";")
+    poggers.commitChanges()
+
+
+def display_tables():
+    poggers.execute("SHOW TABLES")
+
+
+# TODO: Add this test to a separate test file, currently NOT working
+def table_creation_test():
+    print("***Before table creation***")
+    display_tables()
+    print("***After table creation***")
+    create_table("test_table")
+    display_tables()
+    print("***After table deletion***")
+    delete_table("test_table")
+    display_tables()
+
+
+display_accounts(permissions=True)
+#display_accounts()
