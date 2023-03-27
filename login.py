585e96428542d6c869f9f5c4ed2887e0ddc2b024
jam0119 <73720359+jam0119@users.noreply.github.com>
2023-03-27 17:21:36 -0500

    add basic patient info page

--- login.py
+++ login.py
@@ -7,6 +7,96 @@ def run_gui():
     root = Tk()
     root.title("Patient Information Management")
     root.geometry("1280x720")
+
+    # expanded patient info page
+    def open_patient_info():
+        patient_frame = ttk.Frame(root, width=1260, height=700, padding=10, borderwidth=5, relief='solid')
+        patient_frame.pack(pady=10)
+        patient_frame.grid_propagate(False)
+
+        # set up grid
+        patient_frame.columnconfigure(1, weight=1)
+        patient_frame.columnconfigure(2, weight=2)
+        patient_frame.columnconfigure(3, weight=1)
+        patient_frame.columnconfigure(4, weight=2)
+        for i in range(1, 20):
+            patient_frame.rowconfigure(i, weight=1)
+
+        # tier 0 data
+        patient_patient_name_label = ttk.Label(patient_frame, text="Patient Name:", font=('Arial', 16))
+        patient_patient_name_label.grid(row=1, column=1, sticky='nw')
+
+        patient_building_label = ttk.Label(patient_frame, text="Building Location:", font=('Arial', 16))
+        patient_building_label.grid(row=2, column=1, sticky='nw')
+
+        patient_room_number_label = ttk.Label(patient_frame, text="Room Number:", font=('Arial', 16))
+        patient_room_number_label.grid(row=3, column=1, sticky='nw')
+
+        patient_bed_number_label = ttk.Label(patient_frame, text="Bed Number:", font=('Arial', 16))
+        patient_bed_number_label.grid(row=4, column=1, sticky='nw')
+
+        patient_restricted_visitors_label = ttk.Label(patient_frame, text="Restricted Visitors:", font=('Arial', 16))
+        patient_restricted_visitors_label.grid(row=5, column=1, sticky='nw')
+
+        patient_allowed_visitors_label = ttk.Label(patient_frame, text="Allowed Visitors:", font=('Arial', 16))
+        patient_allowed_visitors_label.grid(row=6, column=1, sticky='nw')
+
+        # tier 1 data TODO: only show if account tier >=1
+        patient_admission_reason_label = ttk.Label(patient_frame, text="Admission Reason:", font=('Arial', 16))
+        patient_admission_reason_label.grid(row=7, column=1, sticky='nw')
+
+        patient_admission_date_label = ttk.Label(patient_frame, text="Admission Date:", font=('Arial', 16))
+        patient_admission_date_label.grid(row=8, column=1, sticky='nw')
+
+        patient_discharge_date_label = ttk.Label(patient_frame, text="Discharge Date:", font=('Arial', 16))
+        patient_discharge_date_label.grid(row=9, column=1, sticky='nw')
+
+        patient_mailing_address_label = ttk.Label(patient_frame, text="Mailing Address:", font=('Arial', 16))
+        patient_mailing_address_label.grid(row=10, column=1, sticky='nw')
+
+        patient_phone_number_label = ttk.Label(patient_frame, text="Phone Number:", font=('Arial', 16))
+        patient_phone_number_label.grid(row=11, column=1, sticky='nw')
+
+        patient_emergency_contacts_label = ttk.Label(patient_frame, text="Emergency Contacts:", font=('Arial', 16))
+        patient_emergency_contacts_label.grid(row=12, column=1, sticky='nw')
+
+        patient_family_doctor_label = ttk.Label(patient_frame, text="Family Doctor:", font=('Arial', 16))
+        patient_family_doctor_label.grid(row=13, column=1, sticky='nw')
+
+        patient_insurance_carrier_label = ttk.Label(patient_frame, text="Insurance Carrier:", font=('Arial', 16))
+        patient_insurance_carrier_label.grid(row=14, column=1, sticky='nw')
+
+        patient_insurance_account_number_label = ttk.Label(patient_frame, text="Insurance Account Number:", font=('Arial', 16))
+        patient_insurance_account_number_label.grid(row=15, column=1, sticky='nw')
+
+        patient_insurance_group_number_label = ttk.Label(patient_frame, text="Insurance Group Number:", font=('Arial', 16))
+        patient_insurance_group_number_label.grid(row=16, column=1, sticky='nw')
+
+        patient_insurance_amount_paid_label = ttk.Label(patient_frame, text="Insurance Amount Paid:", font=('Arial', 16))
+        patient_insurance_amount_paid_label.grid(row=17, column=1, sticky='nw')
+
+        patient_patient_amount_paid_label = ttk.Label(patient_frame, text="Patient Amount Paid:", font=('Arial', 16))
+        patient_patient_amount_paid_label.grid(row=18, column=1, sticky='nw')
+
+        patient_patient_amount_owed_label = ttk.Label(patient_frame, text="Patient Amount Owed:", font=('Arial', 16))
+        patient_patient_amount_owed_label.grid(row=19, column=1, sticky='nw')
+
+        patient_history_of_charges_label = ttk.Label(patient_frame, text="History of Charges:", font=('Arial', 16))
+        patient_history_of_charges_label.grid(row=20, column=1, sticky='nw')
+
+        # tier 2/3 data TODO: only show if account tier >=2 (and make only doctors able to edit doctor notes, same for nurses)
+        patient_list_of_prescriptions_label = ttk.Label(patient_frame, text="List of Prescriptions:", font=('Arial', 16))
+        patient_list_of_prescriptions_label.grid(row=1, column=3, sticky='nw')
+
+        patient_list_of_procedures_label = ttk.Label(patient_frame, text="List of Procedures:", font=('Arial', 16))
+        patient_list_of_procedures_label.grid(row=2, column=3, sticky='nw')
+
+        patient_doctors_notes_label = ttk.Label(patient_frame, text="Doctor's Notes:", font=('Arial', 16))
+        patient_doctors_notes_label.grid(row=3, column=3, sticky='nw')
+
+        patient_nurses_notes_label = ttk.Label(patient_frame, text="Nurse's Notes:", font=('Arial', 16))
+        patient_nurses_notes_label.grid(row=12, column=3, sticky='nw')
+
     # landing page, has search bar, results window, various buttons at the bottom
     def open_landing():
         # left frame, for search bar and options (?)
@@ -20,7 +110,6 @@ def run_gui():
         name_entry = ttk.Entry(search_frame, width=40)
         name_entry.pack(anchor='nw', padx=10, pady=10)
         # search button by the name entry bar
-        # todo: fix the placing of this button idfk
 
         def search_button_function():
             # todo: make this query db for what's in name_entry
@@ -65,6 +154,10 @@ def run_gui():
         def patient_select(e):
             # todo: make this go to patient page
             print('click')
+            search_frame.destroy()
+            results_frame.destroy()
+            buttons_frame.destroy()
+            open_patient_info()
 
         def search_for_patients():
             result_list: list[Frame] = list()
