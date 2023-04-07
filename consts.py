5a9af25a48f2321dfb5a87710cc2dd72476da7bb
Michael Kelly <cybernationnews@gmail.com>
2023-04-07 10:22:24 -0500

    Small fix

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 # DO NOT COMMIT THIS LINK TO THE REPO OR ANYONE WHO GRABS IT
 # CAN MESS WITH DATA IN THE DB
 # will look into a more permanent solution.
-HOST = "136.53.87.218"
+HOST = ""
 PORT = 3306
 DATABASE = "patient_information"
