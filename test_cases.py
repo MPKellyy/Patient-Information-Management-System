9debc9c54806bd74b1598d25ce65a7d1879343ce
riley <roketson44@gmail.com>
2023-04-12 13:02:49 -0500

    properly defined testing format and added an example.

--- test_cases.py
+++ test_cases.py
@@ -1,5 +1,7 @@
 from database import *
 
+db = Database()
+
 '''
 HEADER:
 this file is the test case document for this project. here i will define standard procedure as well as 
@@ -11,17 +13,41 @@ database object is defined as 'db'
 
 PROPER TESTING FORMAT:
 
-preamble:
-here you will define what the test function is supposed to be testing, and what it's expected output is, as well
-as any other useful information regarding the function
     def function():
+        preamble:
+        here you will define what the test function is supposed to be testing, and what it's expected output is, as well
+        as any other useful information regarding the function
+        
+        post:
+        here you will define a list of testing history and results. nothing super detailed needs to be here, if output is
+        wrong or an error, describe the issue briefly. if the expected output is achieved, note it.
+        do not include unexpected output or errors not directly tied to the tested functions.
+        
         function here
-
-post:
-here you will define a list of testing history and results. nothing super detailed needs to be here, if output is
-wrong or an error, describe the issue briefly. if the expected output is achieved, note it.
-do not include unexpected output or errors not directly tied to the tested functions.
-
+        
+ITS UGLY AND I HATE IT BUT PYTHON YELLS AT ME OTHERWISE AAAAAAAAAAAAAAAAAAAAAAAA
 '''
 
-def account_create_test():
+
+
+
+def dict_execute_test():
+    '''
+    preamble:
+    this tests that the DB is returning a dictionary when asked to return one. it also tests to see if it goes back to
+    returning tuples when doing normal operations.
+    expected output is a dict, then a tuple.
+
+    post:
+    the results of the test were correct and accurate.
+    '''
+    test = db.dict_execute("SELECT * FROM patient_medical")
+    for i in test:
+        print(type(i))
+    test = db.execute("SELECT * FROM patient_medical")
+    for i in test:
+        print(type(i))
+
+
+db.connect(ADMINUSER, ADMINPASS)
+dict_execute_test()
